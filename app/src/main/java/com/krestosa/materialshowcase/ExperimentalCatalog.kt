e5ac2d42c1986c95a54f49bdd7a30ba67ef77760
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-22 00:59:08 -0300

    Restore dp import for Labs catalog

--- app/src/main/java/com/krestosa/materialshowcase/ExperimentalCatalog.kt
+++ app/src/main/java/com/krestosa/materialshowcase/ExperimentalCatalog.kt
@@ -34,6 +34,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun ExperimentalCatalogContent(
