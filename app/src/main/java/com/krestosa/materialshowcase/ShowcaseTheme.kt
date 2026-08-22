335f1abb83b9123be046d6a7c5a26f3f1a52cce9
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-22 00:51:22 -0300

    Add system and dynamic color theme

---
+++ app/src/main/java/com/krestosa/materialshowcase/ShowcaseTheme.kt
@@ -0,0 +1,28 @@
+package com.krestosa.materialshowcase
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+internal fun ShowcaseTheme(content: @Composable () -> Unit) {
+    val darkTheme = isSystemInDarkTheme()
+    val context = LocalContext.current
+    val colors = when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && darkTheme -> dynamicDarkColorScheme(context)
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> dynamicLightColorScheme(context)
+        darkTheme -> darkColorScheme()
+        else -> lightColorScheme()
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        content = content,
+    )
+}
