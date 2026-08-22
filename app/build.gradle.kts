373bd6aed6d6e7c6fa1897ab7a90de3bd98437f2
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-22 00:51:18 -0300

    Use latest Material 3 alpha for labs catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     androidTestImplementation(composeBom)
 
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.5.0-alpha26")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui")
